17fa8d60019a896ec794ddaf8077a552981224da
Olli Paust <ollipaust@gmail.com>
2023-11-22 16:42:58 +0100

    install core packages + add file structure

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,7 @@ export default function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-      </head>
+      </head>d
       <body>
         <Outlet />
         <ScrollRestoration />
