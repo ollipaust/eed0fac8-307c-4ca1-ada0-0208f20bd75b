69c5c79511144929186245c9dc1d6415073c8638
Olli Paust <ollipaust@gmail.com>
2023-11-24 20:22:42 +0100

    Revert "add netlify interface"
    This reverts commit 0c8cca5521ba4c42d0adc3fc0243b66790df243b.

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,11 @@ import { config } from "@netlify/remix-adapter";
 
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  ignoredRouteFiles: ["**/.*"],
   ...(process.env.NODE_ENV === "production" ? config : undefined),
-  // This works out of the box with the Netlify adapter, but you can
-  // add your own custom config here if you want to.
-  //
-  // See https://remix.run/file-conventions/remix-config
+  // appDirectory: "app",
+  // Add the buildOptions configuration
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  // serverBuildPath: "build/index.js",
 };
