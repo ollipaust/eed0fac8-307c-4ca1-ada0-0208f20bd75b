0f838f090c33fc6bc1659fd91b011a470a4a23e1
Olli Paust <ollipaust@gmail.com>
2023-11-27 04:39:52 +0100

    adjust cart handle + add some responsive scss

--- app/ui/components/eventsGrid/shopCartHandle.tsx
+++ app/ui/components/eventsGrid/shopCartHandle.tsx
@@ -4,7 +4,7 @@ import { useShoppingCartContext } from '~/utils/appContextProvider';
 import TimeFormat from '~/utils/formatDateAndTime';
 
 const ShopCartHandler: React.FC = () => {
-  const { cart, removeFromCart, maxCartItemsSelected  } = useShoppingCartContext();
+  const { cart, removeFromCart, maxCartItemsSelected } = useShoppingCartContext();
   const [isActive, setIsActive] = useState(false);
   const containerRef = useRef<HTMLDivElement | null>(null);
   const maxTitleLength = 35;
@@ -18,10 +18,14 @@ const ShopCartHandler: React.FC = () => {
   };
 
   const handleClickOutside = (event: MouseEvent) => {
+    const clickedOnRemoveBtn = event.target?.closest('#RemoveItemBtn');
+    const clickedOnPlusIcon = event.target?.closest('.SvgCartIconPlus'); // Add the class name of SvgCartIconPlus here
+
     if (
       containerRef.current &&
       !containerRef.current.contains(event.target as Node) &&
-      !document?.getElementById('RemoveItemBtn')
+      !clickedOnRemoveBtn &&
+      !clickedOnPlusIcon
     ) {
       setIsActive(false);
     }
@@ -47,38 +51,36 @@ const ShopCartHandler: React.FC = () => {
         <SvgCartIconFull svgCartType="full" width={32} height={32} cartItemCount={cart.length} />
       </button>
       <div className={`shopCart shopCart--${isActive ? 'active' : 'inactive'}`}>
-        <>
-          <div className='shopCart__header dark'>
-            <b>Total Items in the Cart:&nbsp;</b> 
-            <span>{countedItems}</span>
-            {countedItems < maxCartItemsSelected ? null : <div className='warningText'>Limit of {maxCartItemsSelected} items reached.<br />Please remove items or proceed to Checkout!</div>}
-          </div>
-          {cart.length > 0 ? (
-            <>
-              <ul className='shopCart__itemsList'>
-                {cart.map((item, index) => (
-                  <li className='shopCart__item' key={item._id}>
-                    <div className='dark'>
-                      {`${index + 1}. ${cropTitle(item.title, maxTitleLength)}`}
-                    </div>
-                    <button id="RemoveItemBtn" className="shopCart__removeButton" onClick={() => removeFromCart(item)}>
-                      X
-                    </button>
-                    <div className='dark50 smallText'>
-                      <TimeFormat startTime={item.startTime} endTime={item.endTime} fallBackTime={item.date} />
-                    </div>
-                  </li>
-                ))}
-              </ul>
-              <div className='hr' />
-              <button className="shopCart__payNowButton btn" onClick={handlePayNowClick}>
-                Checkout
-              </button>
-            </>
-          ) : (
-            <div className='shopCart__item--empty dark75'>Nothing here, please buy something.</div>
-          )}
-        </>
+        <div className='shopCart__header dark'>
+          <b>Total Items in your Cart:&nbsp;</b>
+          <span>{countedItems}</span>
+          {countedItems < maxCartItemsSelected ? null : <div className='warningText'>Limit of {maxCartItemsSelected} items reached.<br />Please remove items or proceed to Checkout!</div>}
+        </div>
+        {cart.length > 0 ? (
+          <>
+            <ul className='shopCart__itemsList'>
+              {cart.map((item, index) => (
+                <li className='shopCart__item' key={item._id}>
+                  <div className='dark'>
+                    {`${index + 1}. ${cropTitle(item.title, maxTitleLength)}`}
+                  </div>
+                  <button id="RemoveItemBtn" className="shopCart__removeButton" onClick={() => removeFromCart(item)}>
+                    X
+                  </button>
+                  <div className='dark50 smallText'>
+                    <TimeFormat startTime={item.startTime} endTime={item.endTime} fallBackTime={item.date} />
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <div className='hr' />
+            <button className="shopCart__payNowButton btn" onClick={handlePayNowClick}>
+              Checkout
+            </button>
+          </>
+        ) : (
+          <div className='shopCart__item--empty dark75'>No items added yet.</div>
+        )}
       </div>
     </div>
   );
